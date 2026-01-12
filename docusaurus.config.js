0a207804b0ae2491aae7431ddf42f2b9edbb516e
Nabila Sharif <161031798+nabila-sharif@users.noreply.github.com>
2026-01-12 20:03:36 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url:'https://ai-book-ashen-chi.vercel.app',
+  url: 'https://new-book-delta.vercel.app',
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
